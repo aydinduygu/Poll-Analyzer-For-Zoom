e3cae4425aa16e032305248a2b2fcb6101f45538
Aydın DUYGU <62114034+aydinduygu@users.noreply.github.com>
2021-01-13 18:44:54 +0300

    printStudentList method added

--- PollAnalyzer.py
+++ PollAnalyzer.py
@@ -10,8 +10,12 @@ class PollAnalyzer:
         filePath1 = "CES3063_Fall2020_rptSinifListesi.XLS"
         filePath2 = "CSE3063_20201123_Mon_zoom_PollReport.csv"
         parser = Parser(filePath1, filePath2)
-
         self.__studentList = parser.parseStudentList()
+
+        self.printStudentList()
 
     def getStudentList(self):
             return self.__studentList
+
+    def printStudentList(self):
+            print(*self.__studentList, sep='\n')
