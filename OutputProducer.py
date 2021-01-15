58586fb9cc21944d822addbf58eeb572ed3051dc
berkyildiz52 <berkyildiz5299@gmail.com>
2021-01-15 19:14:30 +0300

    logIntoExecutionFile method is prepared

--- OutputProducer.py
+++ OutputProducer.py
@@ -1,2 +1,17 @@
+from datetime import datetime
+
+
 class OutputProducer:
-    pass
+
+    basicConfig = None
+
+    def addIntoExecutionLog(self, logInfo):
+
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        f = open("logFile.txt", "a")
+        f.write(dt_string + " "  + logInfo + '\n')
+        f.close()
+
+        print(dt_string + " "  + logInfo + '\n')
